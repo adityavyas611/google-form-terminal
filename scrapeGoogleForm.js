0a6594d06d85acb56105de565d4e35c5a2f49333
Aditya Vyas <vyasaditya41@yahoo.in>
2020-01-02 20:41:52 +0530

    Google form scrapeGoogleform module

--- scrapeGoogleForm.js
+++ scrapeGoogleForm.js
@@ -1,10 +1,10 @@
 const request = require("request");
 const extractQuestions = require("./extractQuestions");
 
-const scrapeGoogleForm = (form_id) => {
+const scrapeGoogleForm = () => {
   return new Promise((resolve, reject) => {
     request(
-    `https://docs.google.com/forms/d/e/${form_id}/viewform`,
+    `https://docs.google.com/forms/d/e/${process.argv[2]}/viewform`,
     (error, response, body) => {
       if (error) {
         console.error("Something went wrong");
