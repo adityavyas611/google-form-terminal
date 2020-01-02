fa1f464297d088aab996a98f4315cd56a2b5a0f6
Aditya Vyas <vyasaditya41@yahoo.in>
2020-01-02 20:16:03 +0530

    Google forms return questions

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const scrapeGoogleForm = require("./scrapeGoogleForm");
 
-module.exports = scrapeGoogleForm;
+const form_id = process.argv[2];
+
+const getQuestion = async () => {
+    const questions = await scrapeGoogleForm(form_id);
+    return questions;
+};
+
+module.exports = getQuestion;
--- scrapeGoogleForm.js
+++ scrapeGoogleForm.js
@@ -1,23 +1,24 @@
 const request = require("request");
 const extractQuestions = require("./extractQuestions");
 
-const scrapeGoogleForm = () => {
-  const form_id = process.argv[2];
-  request(
+const scrapeGoogleForm = (form_id) => {
+  return new Promise((resolve, reject) => {
+    request(
     `https://docs.google.com/forms/d/e/${form_id}/viewform`,
     (error, response, body) => {
       if (error) {
         console.error("Something went wrong");
       } else {
-        let questions = extractQuestionsFromBody(body);
-        if (questions) {
-          return questions;
+        let questions = extractQuestions(body);
+        if (questions.length) {
+           resolve(questions);
         } else {
           console.error("Ah! I couldn't find the questions.");
         }
       }
     }
   );
+  })
 };
 
 module.exports = scrapeGoogleForm;
